24212b5182e18bcd3347ff656cd3b75129977798
Raja Aliyan <aliyanr5421@gmail.com>
2026-06-28 00:41:41 +0500

    changed

--- loops.py
+++ loops.py
@@ -102,3 +102,123 @@ def print_range(start, end):
 print_range(1, 5)
 
 
+
+
+
+
+       # FOR LOOP
+for x in range(5): # range of numbers will start from 0 by default and the range of number will be one less than the given value
+   print(x)          #range(stop)
+
+
+
+
+
+product=1
+for n in range(1,10):     #range(start,stop)
+   product=product*n
+print(product)
+
+
+
+
+friends =['taylor','alex','eli']
+for friend in friends:
+   print("Hi ",friend)
+
+
+
+
+
+
+values=[0,5,4,33,5,55,3]
+sum=0
+length=0
+for value in values:
+   sum+=value
+   length+=1
+print("Total sum= ",str(sum),"-Average= ",str(sum/length))
+
+
+
+
+
+
+
+
+def to_celcius(x):
+   return(x-32)*5/9
+
+for x in range(0,101,10):    # 0 lower limit hay or 101 end wali or hamna pehla kaha tha last element sa pichli value par rukta loop means 100 par ruka ga or 10 ka gap hoga
+   print(x,to_celcius(x))  #range(start, stop, step) 
+
+
+
+
+
+
+
+
+
+# This loop iterates on the value of the "n" variable in a range
+# of 0 to 10 (the value of the end-of-range index 11 is excluded).
+# The incremental value for the loop is 2. The print() function will 
+# output the resulting value of "n" as the loop counts from 0 to 10 
+# (end-of-range index 11) in incremental steps of 2. This is one 
+# method that can be used in Python to print a list of even numbers.
+
+
+for n in range(0,11,2):
+    print(n)
+
+
+# The loop should print 0, 2, 4, 6, 8, 10
+
+
+
+
+
+
+
+
+
+
+            # NESTED FOR LOOP
+
+for left in range(7):
+   for right in range(left,7):
+      print("[",str(left),"|"," ",str(right),"]",end=" ")
+      print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+teams=['dragons','wolves','pandas','unicorns']
+for home_team in teams:
+   for away_team in teams:
+      if home_team!=away_team:
+         print(home_team ,"vs", away_team)
+
+
+
+
+
+
+
+
+
+         # strings in for loop
+
+greeting="hello"
+for c in greeting:
+   print("the character is:",c)
+   
